f216a969964d8e6403ccb27e420bfa5928f6c903
Hugo <martin.romero.hugo@iescamas.es>
2024-10-03 15:40:29 +0200

    App terminada

--- app/src/main/java/com/fireboy/mevoydeviaje/MainActivity.java
+++ app/src/main/java/com/fireboy/mevoydeviaje/MainActivity.java
@@ -1,18 +1,53 @@
 package com.fireboy.mevoydeviaje;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioGroup rgPaises, rgTransportes;
+    Button btnInfo;
+
+    String transporte, pais;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rgPaises = findViewById(R.id.rgPaises);
+        rgTransportes = findViewById(R.id.rgTransportes);
+        btnInfo = findViewById(R.id.button);
+
+        rgPaises.setOnCheckedChangeListener((group, checkedId) -> {
+            RadioButton rb = findViewById(checkedId);
+            if (rb != null) {
+                pais = rb.getText().toString();
+            }
+        });
+
+        rgTransportes.setOnCheckedChangeListener((group, checkedId) -> {
+            RadioButton rb = findViewById(checkedId);
+            if (rb != null) {
+                transporte = rb.getText().toString();
+            }
+        });
+
+        btnInfo.setOnClickListener(v -> info());
+    }
+
+    @SuppressLint("ShowToast")
+    private void info() {
+        if (transporte != null && pais != null) {
+            Toast.makeText(getApplicationContext(), "Vas a viajar a " + pais + " en " + transporte + ".", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Por favor, selecciona país y transporte.", Toast.LENGTH_LONG).show();
+        }
     }
 }
